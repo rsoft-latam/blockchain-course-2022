d4a3556c4778c234c82a3ea32153096966f92cf3
Ricardo Pari <rikiricardo16@hotmail.com>
2022-05-20 14:10:40 -0400

    how to use require in solidity

--- Inbox.sol
+++ Inbox.sol
@@ -1,11 +1,12 @@
-// SPDX-License-Identifier: GPL-3.0
 pragma solidity  ^0.8.0;
 
 contract Inbox {
 
     string public message;
+    address private ownerAddress;
 
     constructor(string memory initialMessage) {
+        ownerAddress = msg.sender;
         message = initialMessage;
     }
 
@@ -14,6 +15,9 @@ contract Inbox {
     }
 
     function setMessage(string memory newMessage) public {
+
+        //require( bytes(newMessage).length > 10 , "El mensage tiene que tener mas de 10 caracteres." ) ; 
+        require( msg.sender == ownerAddress , "Solo el Owner puede modificar el mensaje." ) ;
         message = newMessage;
     }
 
