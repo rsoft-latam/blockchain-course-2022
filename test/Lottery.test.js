c1900476a3e9af87d4403b6e120a09b774e06cdb
Ricardo Pari <rikiricardo16@hotmail.com>
2022-06-08 12:41:40 -0400

    [08-06-2022] task1 use before each and 2do unit testing

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,13 +1,28 @@
 const Lottery = artifacts.require("Lottery");
 
 contract("Lottery", accounts=> {
+    let instance;
+    beforeEach("deploys a contract", async () => {
+        instance = await Lottery.new();
+    })
 
     it("allows one account to enter", async ()=> {
-        const instance = await Lottery.deployed();
         await instance.enter({from: accounts[1], value: web3.utils.toWei("3", "ether")});
         const players = await instance.getPlayers.call();
         assert.equal(accounts[1], players[0])
         assert.equal(1, players.length)
     })
 
+    it("allows multiple account to enter", async ()=> {
+        await instance.enter({from: accounts[0], value: web3.utils.toWei("3", "ether")})
+        await instance.enter({from: accounts[1], value: web3.utils.toWei("4", "ether")})
+        await instance.enter({from: accounts[2], value: web3.utils.toWei("5", "ether")})
+
+        const players = await instance.getPlayers.call()
+
+        assert.equal(accounts[0], players[0])
+        assert.equal(accounts[1], players[1])
+        assert.equal(accounts[2], players[2])
+        assert.equal(3, players.length);
+    })
 })
